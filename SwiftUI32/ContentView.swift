7e395a49805123ea668bad0c2c3014503e8cb42e
Rohit Saini <sketchlearn7@gmail.com>
2020-08-09 17:25:30 +0530

    Code Updated

--- SwiftUI32/ContentView.swift
+++ SwiftUI32/ContentView.swift
@@ -9,8 +9,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var search = ""
+    @State var show = false
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            HStack{
+                if !show{
+                   Text("Food").fontWeight(.heavy).foregroundColor(.white)
+                }
+                
+                Spacer(minLength: 0)
+                HStack{
+                    if show{
+                        Image(systemName: "magnifyingglass")
+                        TextField("Search", text: $search)
+                        Button(action: {
+                            withAnimation() {
+                                self.show.toggle()
+                            }
+                        }) {
+                            Image(systemName: "xmark").foregroundColor(.black)
+                        }
+                    }
+                    else{
+                        Button(action: {
+                            withAnimation() {
+                                self.show.toggle()
+                            }
+                        }) {
+                            Image(systemName: "magnifyingglass").foregroundColor(.black).padding(10)
+                        }
+                    }
+                }
+                .padding(show ? 10: 0)
+                .background(Color.white)
+                .cornerRadius(20)
+                
+                
+            }
+            .padding(.top,(UIApplication.shared.windows.first?.safeAreaInsets.top)! + 15)
+            .padding(.horizontal)
+            .padding(.bottom,10)
+            .background(Color.orange)
+            Spacer()
+            
+        }
+        
     }
 }
 
@@ -19,3 +63,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
